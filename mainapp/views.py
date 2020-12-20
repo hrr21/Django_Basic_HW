0e4a79c6be1a382717cd3f2962206f70a82e83f8
hrr21 <hrr21@yandex.ru>
2020-12-21 01:02:22 +0300

    Create HW for Lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
 
 
 def index(request):
-    return render(request,'mainapp/index.html')
+    return render(request, 'mainapp/index.html')
 
 
 def products(request):
-    return render(request,'mainapp/products.html')
+    context = {
+        'products': Product.objects.all(),
+
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
 
 def test_context(request):
     context = {
@@ -21,7 +28,5 @@ def test_context(request):
             {'name': 'Туфли Dr Martins', 'price': '10 000 руб.'},
         ],
     }
-
-
 
     return render(request, 'mainapp/context.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 
@@ -25,3 +28,5 @@ urlpatterns = [
     path('products/', mainapp_views.products, name='products'),
     path('test_context/', mainapp_views.test_context)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
